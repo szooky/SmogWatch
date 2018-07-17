5bcd26baf6b1a5d04c5a78693d99d16c42b99b71
Filip Szukała <filip.szukala@itmagination.com>
2018-07-17 18:05:35 +0200

    Initial Commit

---
+++ SmogWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmogWatch
+//
+//  Created by Filip Szukala on 17/07/2018.
+//  Copyright © 2018 Filip Szukala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
